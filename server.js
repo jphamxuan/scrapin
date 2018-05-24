3ac3e4dd1addfa6aaf78c1201c37a2365dfa1a17
jphamxuan <34908420+jphamxuan@users.noreply.github.com>
2018-05-23 18:22:52 -0700

    added summaries, still need notes and polulate home page

--- server.js
+++ server.js
@@ -1,73 +1,69 @@
-//dependencies
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-var request = require('request');
-var cheerio = require('cheerio');
+// Dependencies
+var express = require("express");
 var mongojs = require("mongojs");
+// Require request and cheerio. This makes the scraping possible
+var request = require("request");
+var cheerio = require("cheerio");
 
-//use body parser
-app.use(bodyParser.urlencoded({
-	extended: false
-}));
+// Initialize Express
+var app = express();
 
-//use public folder
-app.use(express.static('public'));
+// Database configuration
+var databaseUrl = "scraper";
+var collections = ["scrapedData"];
 
-//mongoose connection
-mongoose.connect('');
-// mongoose.connect('mongodb://localhost/onionScraper');
-var db = mongoose.connection;
-
-//mongo error handling
-db.on('error', function(err) {
-	console.log('Mongoose Error: ', err);
+// Hook mongojs configuration to the db variable
+var db = mongojs(databaseUrl, collections);
+db.on("error", function(error) {
+  console.log("Database Error:", error);
 });
 
-//schemas models
-var Note = require('./models/Note.js');
-var Article = require('./models/Article.js');
-
-//routes
-app.get('/', function(req, res) {
-	res.send(index.html);
+// Main route (simple Hello World Message)
+app.get("/", function(req, res) {
+  res.render('index');
 });
 
-//get data from DB
+// Retrieve data from the db
 app.get("/all", function(req, res) {
-	// Find all results from the scrapedData collection in the db
-	db.scrapedData.find({}, function(error, found) {
-	  // Throw any errors to the console
-	  if (error) {
-		console.log(error);
-	  }
-	  // If there are no errors, send the data to the browser as json
-	  else {
-		res.json(found);
-	  }
-	});
+  // Find all results from the scrapedData collection in the db
+  db.scrapedData.find({}, function(error, found) {
+    // Throw any errors to the console
+    if (error) {
+      console.log(error);
+    }
+    // If there are no errors, send the data to the browser as json
+    else {
+      res.json(found);
+    }
   });
-  
-  //scrape from website
-  app.get("/scrape", function(req, res) {
+});
+
+// Scrape data from one site and place it into the mongodb db
+app.get("/scrape", function(req, res) {
 	// Make a request for the news section of `ycombinator`
 	request("https://www.nytimes.com/", function(error, response, html) {
 	  // Load the html body from request into cheerio
 	  var $ = cheerio.load(html);
-	  // For each element with a "story-heading" class
-	  $("h2.story-heading").each(function(i, element) {
+	  // For each element with a "title" class
+	  $("article.story").each(function(i, element) {
 		// Save the text and href of each link enclosed in the current element
-		var title = $(element).children("a").text();
-		var link = $(element).children("a").attr("href");
-		// var link = $(element).children("a").attr("href");
+		var title = $(this)
+				.children("h2.story-heading")
+				.children("a")
+				.text();
+		var link = $(this)
+				.children("h2.story-heading")
+				.children("a")
+				.attr("href");
+		var summary = $(this)
+			.children("p.summary")
+				.text();
   
-		// If this found element had both a title and a link
-		if (title) {
-		  // Insert the data in the scrapedData db
+		//adds data to DB
 		  db.scrapedData.insert({
 			title: title,
-			// link: link
+      link: link,
+      summary: summary,
 		  },
 		  function(err, inserted) {
 			if (err) {
@@ -79,19 +75,31 @@ app.get("/all", function(req, res) {
 			  console.log(inserted);
 			}
 		  });
-		}
-	  });
+		
+		});
+		
 	});
 
-	  // Send a "Scrape Complete" message to the browser
-	  res.send("Scrape Complete");
-	});
-	
-	
-	// Listen on port 3000
-	app.listen(3000, function() {
-	  console.log("App running on port 3000!");
-	});
-	
-  
+  // Send a "Scrape Complete" message to the browser
+  res.send("Scrape Complete");
+});
 
+// Route for getting all Articles from the db
+app.get("/articles", function(req, res) {
+  // Grab every document in the Articles collection
+  db.Article.find({})
+    .then(function(dbArticle) {
+      // If we were able to successfully find Articles, send them back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+
+// Listen on port 3000
+app.listen(3000, function() {
+  console.log("App running on port 3000!");
+});
