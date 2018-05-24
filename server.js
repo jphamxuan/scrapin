a5694da5f66215b947601d37d2957ffcf62795ab
jphamxuan <34908420+jphamxuan@users.noreply.github.com>
2018-05-23 19:49:37 -0700

    fixed home page loading issue

--- server.js
+++ server.js
@@ -12,6 +12,11 @@ var app = express();
 var databaseUrl = "scraper";
 var collections = ["scrapedData"];
 
+app.use(express.static('public'));
+
+//mongoose connection
+mongoose.connect("mongodb://localhost/scrapin");
+
 // Hook mongojs configuration to the db variable
 var db = mongojs(databaseUrl, collections);
 db.on("error", function(error) {
@@ -20,7 +25,7 @@ db.on("error", function(error) {
 
 // Main route (simple Hello World Message)
 app.get("/", function(req, res) {
-  res.render('index');
+  res.send(public/index.html);
 });
 
 // Retrieve data from the db
@@ -38,15 +43,15 @@ app.get("/all", function(req, res) {
   });
 });
 
-// Scrape data from one site and place it into the mongodb db
+// Scrape data from NYT to  mongodb db
 app.get("/scrape", function(req, res) {
-	// Make a request for the news section of `ycombinator`
+	
 	request("https://www.nytimes.com/", function(error, response, html) {
 	  // Load the html body from request into cheerio
 	  var $ = cheerio.load(html);
-	  // For each element with a "title" class
+	  
 	  $("article.story").each(function(i, element) {
-		// Save the text and href of each link enclosed in the current element
+	
 		var title = $(this)
 				.children("h2.story-heading")
 				.children("a")
